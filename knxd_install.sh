6e988ca9f5da19483230aae644aa5284e483e486
Mauro Ferratello <mauro.ferratello@lastminute.com>
2017-03-04 19:06:42 +0100

    + Added script file + Improved README

---
+++ knxd_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get install git-core build-essential dh-systemd
+git clone https://github.com/knxd/knxd.git
+
+# knxd requires libpthsem which unfortunately isn't part of Debian
+wget https://www.auto.tuwien.ac.at/~mkoegler/pth/pthsem_2.0.8.tar.gz
+tar xzf pthsem_2.0.8.tar.gz
+cd pthsem-2.0.8
+dpkg-buildpackage -b -uc
+cd ..
+sudo dpkg -i libpthsem*.deb
+
+# now build+install knxd itself
+cd knxd
+git checkout stable ## this branch
+dpkg-buildpackage -b -uc
+cd ..
+sudo dpkg -i knxd_*.deb knxd-tools_*.deb
